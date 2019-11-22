54011aeb8bb82f9fb6c30759945f918199bee695
mykparmar007 <mayank.parmar@innovaccer.com>
2019-11-22 15:55:06 +0530

    added more

--- temp.py
+++ temp.py
@@ -1,1 +1,8 @@
-print('Hello Worldkbvskbvjksbbhkfb jhkjd bdcbjk kjndajk bekbkhdbshbfvhs jbvsdhkbskhvbskhvb bkhdbckhbdkhb dnbkbvshkb dabkhbkhdbk dvajkbkjvsbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab  sbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab sbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab sbd  nvb kdbch dms msdb sdvbsj          vbjhvbdsjhv sj vshbdsjhb vsdjh vdsjh djvhbsd jhvs dakbvkdb kvdabkdbvk adkb  dskvbkv meadkhbdkhbs  kjbdkab ')
+print('Hello Worldkbvskbvjksbbhkfb jhkjd bdcbjk kjndajk bekbkhdbshbfvhs jbvsdhkbskhvbskhvb bkhdbckhbdkhb dnbkbvshkb dabkhbkhdbk dvajkbkjvsbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab  sbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab sbd nvbdakbvkdb kvdabkdbvk adkbdskvbkv meadkhbdkhbs kjbdkab sbd  nvb kdbch dms msdb s  dvbsj          vbjhvbdsjhv sj vshbdsjhb vsdjh vdsjh djvhbsd jhvs dakbvkdb kvdabkdbvk adkb  dskvbkv meadkhbdkhbs  kjbdkab ')
+def a(**args):
+    for i,j in args:
+        print(f'{i} costs {j}')
+
+
+
+a('apple'=100,'banana'=1001)
